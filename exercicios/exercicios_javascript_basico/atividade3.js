3e2e47548dfff99196a22fde8b8703814406f5f3
Eduardo Froelich <eduardofroelich.dev@gmail.com>
2021-01-16 03:47:40 -0300

    Adding new repository

---
+++ exercicios/exercicios_javascript_basico/atividade3.js
@@ -0,0 +1,10 @@
+// 03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+
+function expoente(base, expoente){
+    resultado = base ** expoente
+    return resultado
+}
+
+console.log(expoente(3, 7))
+console.log(expoente(2, 8))
+
